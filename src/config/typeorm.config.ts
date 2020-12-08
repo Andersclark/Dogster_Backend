c1c978c6a43dd778936136c1af8bd08a9640d73b
Andersclark <anders.clark@gmail.com>
2020-12-08 11:15:46 +0100

    Configs are now set in yml-files with config-package. Sensitive variables will be overwritten with environment variables in production

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,15 @@
 import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import * as config from 'config';
 
+const dbConfig = config.get('db');
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'n5irk*lSYMxn8xr2ChL#$BQI9Rdya2WF2yG1ysV',
-  database: 'dogster',
+  // TODO: Change process.env-variables to point to variables in production.
+  type: dbConfig.type,
+  host: process.env.DB_HOST || dbConfig.host,
+  port: process.env.DB_PORT || dbConfig.port,
+  username: process.env.DB_USERNAME || dbConfig.username,
+  password: process.env.DB_PASSWORD || dbConfig.password,
+  database: process.env.DB_NAME || dbConfig.database,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: true,
+  synchronize: process.env.TYPEORM_SYNC || dbConfig.synchronize, // TODO: set to true for first deploy, then switch (that way we don't have to manually configure schema).
 };
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  const serverConfig = config.get('server');
+
   if(process.env.NODE_ENV === 'development') {
+    logger.log(`CORS: enabled`);
     app.enableCors();
   }
-  const port = 3000;
+  const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}...`);
 }
