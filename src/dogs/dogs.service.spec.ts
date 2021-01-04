e48c042dbc4278e68d9d173ed08e07f26e6bfc31
Andersclark <anders.clark@gmail.com>
2021-01-04 11:43:09 +0100

    Tests for getDogbyId() added and passing.

--- src/dogs/dogs.service.spec.ts
+++ src/dogs/dogs.service.spec.ts
@@ -2,8 +2,16 @@ import { Test } from '@nestjs/testing';
 import { DogsService } from "./dogs.service";
 import { DogRepository } from "./dog.repository";
 import { GetDogsFilterDTO } from "./dto/get-dogs-filter.dto";
+import { NotFoundException } from "@nestjs/common";
 
-const mockuser = { username: 'TestUser', }
+const mockUser = { id: 12, username: 'TestUser', }
+const mockDog = {
+  id: 1,
+  name: "name",
+  description: "description",
+  city: "city",
+  area: "area"
+};
 const mockDogRepository = () => ({
   // method will be mocked by Jest and calls can later be evaluated.
   getDogs: jest.fn(),
@@ -33,13 +41,26 @@ describe('Dogsservice', () => {
       dogRepository.getDogs.mockReturnValue('someValue') ;
       expect(dogRepository.getDogs).not.toHaveBeenCalled();
       const filters: GetDogsFilterDTO = {city: 'City', area: 'Area', owner: 'Owner', search: ''};
-      const result = await dogsService.getDogs(filters, mockuser);
+      const result = await dogsService.getDogs(filters, mockUser);
 
       expect(dogRepository.getDogs).toHaveBeenCalled();
       expect(result).toEqual('someValue');
     })
   });
-  describe('getDogById', async () => {
-    dogRepository.findOne.mockReturnValue({id: 1, name: 'name', description: 'description', city: 'city', area: 'area'})
-  })
+  describe('getDogById', () => {
+    it('call dogRepository.findOne() and successfully retrieve and return the dog',async () => {
+      dogRepository.findOne.mockReturnValue(mockDog);
+      const result = await dogsService.getDogById(1, mockUser);
+      expect(result).toEqual(mockDog);
+      expect(dogRepository.findOne).toHaveBeenCalledWith({
+        where: {
+          id: 1,
+          userId: mockUser.id
+      }})
+    });
+    it('throws an error when task is not found', () => {
+      dogRepository.findOne.mockResolvedValue(null);
+      expect(dogsService.getDogById(1, mockUser)).rejects.toThrow(NotFoundException);
+    })
+  });
 });
