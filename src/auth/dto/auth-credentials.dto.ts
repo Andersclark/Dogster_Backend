692583734529d145cc512f47a958669f747d8a85
Andersclark <anders.clark@gmail.com>
2021-01-05 10:48:10 +0100

    AuthCredentials now require username of 4-30 characters, password of 12-99 characters. RegExp-validation removed.

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -2,14 +2,12 @@ import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
 export class AuthCredentialsDTO {
   @IsString()
-  @MinLength(4)
-  @MaxLength(30)
+  @MinLength(4, { message: 'username has to be longer than 4 characters'})
+  @MaxLength(30, { message: 'username cannot be longer than 30 characters'})
   username: string;
 
   @IsString()
-  @MinLength(8)
-  @MaxLength(99)
-  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-    { message: 'password too weak'})
+  @MinLength(12, { message: 'password too short (minimum 12 characters)'})
+  @MaxLength(99, { message: 'password too long (maximum 99 characters)'})
   password: string;
 }
