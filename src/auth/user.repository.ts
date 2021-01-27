9ff72a50dd75a4583de9c0790c62db26f16474f3
Andersclark <anders.clark@gmail.com>
2021-01-27 11:04:33 +0100

    changed userRepo hashPassword-method to static

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -12,7 +12,7 @@ export class UserRepository extends Repository<User> {
     const user = this.create();
     user.username = username;
     user.salt = await bcrypt.genSalt();
-    user.password = await this.hashPassword(password, user.salt);
+    user.password = await UserRepository.hashPassword(password, user.salt);
     try {
       await user.save();
     } catch(error) {
@@ -33,7 +33,7 @@ export class UserRepository extends Repository<User> {
       return null;
     }
   }
-  private async hashPassword(password: string, salt: string): Promise<string> {
+  private static async hashPassword(password: string, salt: string): Promise<string> {
     return bcrypt.hash(password, salt);
   }
 }
