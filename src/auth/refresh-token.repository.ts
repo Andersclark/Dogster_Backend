3c1b3118379578fdbdcb5b7c610b23f929e53498
Andersclark <anders.clark@gmail.com>
2021-02-01 16:35:52 +0100

    Removed unused methods.

--- src/auth/refresh-token.repository.ts
+++ src/auth/refresh-token.repository.ts
@@ -4,13 +4,9 @@ import { EntityRepository, Repository } from "typeorm";
 @EntityRepository(RefreshToken)
 export class RefreshTokensRepository extends Repository<RefreshToken>  {
 
-  async findTokenByUserId (userId: string): Promise<RefreshToken | null> {
-    return await RefreshToken.findOne({ userId })
-  }
   async findTokenById (id: string): Promise<RefreshToken | null> {
     return await RefreshToken.findOne({ id })
   }
-
   async revokeTokenForUser (userId: string): Promise<RefreshToken> {
       const token = await this.findOne({ userId });
       if(token) {
@@ -19,9 +15,4 @@ export class RefreshTokensRepository extends Repository<RefreshToken>  {
       }
       return;
     }
-  async revokeTokenWithId (tokenId: string): Promise<RefreshToken> {
-    const token = await RefreshToken.findOne({ id: tokenId });
-    token.isRevoked = true;
-    return token.save();
-  }
 }
